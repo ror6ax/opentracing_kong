a7b62493f6884e05f13a2d93d699890e7de9aa77
Gregory Reshetniak <ror6ax@gmail.com>
2018-03-02 15:28:34 +0100

    added some c calls and access lua code

--- kong/plugins/ot/handler.lua
+++ kong/plugins/ot/handler.lua
@@ -1,2 +1,21 @@
 local ffi = require "ffi"
 local lib = ffi.load "./libopentracing.so"
+
+local access = require "kong.plugins.ot.access"
+
+local OtKong = BasePlugin:extend()
+
+function OtKong:new()
+  KongInjection.super.new(self, "otkong")
+  ffi.include "opentracing/dynamic_load.h"
+  ffi.cdef[[
+  typedef struct Config Config;
+  auto load_config = opentracing::DynamicallyLoadTracingLibrary(char*);
+  ]]
+  ffi.C.load_config("\"sampler\": {\"type\": \"const\",\"param\": 1,},\"logging\": True")
+end
+
+function OtKong:access(conf)
+  OtKong.super.access(self)
+  access.execute(conf)
+end
